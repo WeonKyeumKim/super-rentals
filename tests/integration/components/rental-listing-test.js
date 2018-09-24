37ff65e1f756e17d4896775340545823e864a985
weonkyeumkim <kim50311@gmail.com>
2018-09-24 16:31:13 -0700

    9/26/2018 component integration test complete

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -1,26 +1,43 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import EmberObject from '@ember/object';
 
 module('Integration | Component | rental-listing', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{rental-listing}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#rental-listing}}
-        template block text
-      {{/rental-listing}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  hooks.beforeEach(function() {
+    this.rental = EmberObject.create({
+      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+      title: 'test-title',
+      owner: 'test-owner',
+      type: 'test-type',
+      city: 'test-city',
+      bedrooms: 3
+    });
   });
+
+  test('should display rental details', async function(assert) {
+    /* component 의 output 을 just verify 한다. */
+    await render(hbs`{{rental-listing rental=rental}}`);
+    /* expected result from querySelector is 'test-title' */
+    assert.equal(this.element.querySelector('.listing h3').textContent.trim(), 'test-title');
+    /* trim() removes space between Owner: and test-owner */
+    assert.equal(this.element.querySelector('.listing .owner').textContent.trim(), 'Owner:test-owner');
+  });
+
+  test('should toggle wide class on click', async function(assert) {
+    /* component 의 output 을 verify 한다. */
+    await render(hbs`{{rental-listing rental=rental}}`);
+    /* expected result 는 small image 이다. */
+    assert.notOk(this.element.querySelector('.image.wide'), 'initially rendered small');
+    await click('.image');
+    /* expected result 는 wide image 이다. */  
+    assert.ok(this.element.querySelector('.image.wide'), 'rendered wide after click');
+    await click('.image');
+    assert.notOk(this.element.querySelector('.image.wide'), 'rendered small after second click');
+
+  });
+
 });
